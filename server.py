4a323d76f11addfb7543b1bb3260f10b5599d4fe
hitsuper <hitsuper901@gmail.com>
2024-10-24 00:17:13 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_ip = socket.gethostbyname(socket.gethostname())
+    server_port = 12345
+
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(5)
+    print(f"Server listening on {server_ip}:{server_port}")
+
+    client_sockets = []
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_sockets.append(client_socket)
+        print(f"Connection established with {client_address}")
+
+        # Threading to handle multiple clients
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_sockets))
+        client_thread.start()
+
+def handle_client(client_socket, client_sockets):
+    while True:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            break
+        for cs in client_sockets:
+            cs.send(f"{client_socket.getpeername()}: {message}".encode())
+
+start_server()
